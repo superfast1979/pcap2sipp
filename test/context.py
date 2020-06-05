8a70ee7dcb068338260c0ffe18b9acd354acceb2
augello <augello@ITMI00DW2267.corp.dom>
2020-06-05 23:44:39 +0200

    Correct path.Only 2.7

--- test/context.py
+++ test/context.py
@@ -6,6 +6,5 @@ Created on 5 giu 2020
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 import pcap2sipp
