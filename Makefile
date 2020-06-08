8adb3bdc20234939ec48504ec93c52d11b0e8200
augello <augello@ITMI00DW2267.corp.dom>
2020-06-09 00:17:26 +0200

    test

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 init:
-    pip install -r requirements.txt
+	pip install -r requirements.txt
 
 test:
-    py.test tests
+	pytest tests
 
 .PHONY: init test
--- tests/context.py
+++ tests/context.py
@@ -6,5 +6,5 @@ Created on 5 giu 2020
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../pcap2sipp/')))
 import pcap2sipp
