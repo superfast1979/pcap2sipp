2873bd5b82d44899d291982e4d6eeabd90eb8e31
augello <augello@ITMI00DW2267.corp.dom>
2020-06-17 00:02:45 +0200

    Added assertValidPackets Added getClientServerIpFromFirstPacket

--- pcap2sipp/pcap_helper.py
+++ pcap2sipp/pcap_helper.py
@@ -16,11 +16,23 @@ def filterPacketsByCallid(packets, callid):
             filteredPackets.append(packet)
     return filteredPackets, len(filteredPackets)
 
-def pcapHandler(packets, callid):
-    filteredPackets, howManyPackets = filterPacketsByCallid(packets, callid)
 #         sport = packet[scapy_layers.UDP].sport
 #         dport = packet[scapy_layers.UDP].dport
 #         if (sport == 5060 or dport == 5060):
+
+
+def getClientServerIpFromFirstPacket(packet):
+    return packet[scapy_layers.IP].src, packet[scapy_layers.IP].dst
+
+def assertValidPackets(callid, howManyPackets):
+    if howManyPackets == 0:
+        print "Call-Id: {} not in pcap".format(callid)
+        exit(0)
+
+def pcapHandler(packets, callid):
+    filteredPackets, howManyPackets = filterPacketsByCallid(packets, callid)
+    assertValidPackets(callid, howManyPackets)
+    clientIp, serverIp = getClientServerIpFromFirstPacket(filteredPackets[0])
     print(len(filteredPackets))
     for packet in filteredPackets:
-        print(packet.load)
+        scapy.ls(packet)
--- tests/pcap_helper_test.py
+++ tests/pcap_helper_test.py
@@ -14,14 +14,7 @@ class Test(unittest.TestCase):
     @pytest.mark.skip(reason="does not run on linux")
     def test_parsePcap_typical(self):
         pcap_helper.parsePcap("./example.pcap")
-            
-    @pytest.mark.skip(reason="does not run on linux")
-    def test_pcapHandler_typical(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        c = [a,b]
-        pcap_helper.pcapHandler(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
-            
+
     def test_isCallIdInPacket_when_True(self):
         a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         self.assertTrue(pcap_helper.isCallIdInPacket(a, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186"))
@@ -54,5 +47,38 @@ class Test(unittest.TestCase):
         self.assertEqual(0, num_filtered_packets)
         self.assertEqual([], filtered_packets)
 
+    def test_assertValidPackets_when_noPacketsMatched(self):
+        with self.assertRaises(SystemExit) as se:
+            pcap_helper.assertValidPackets("sdas0q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186", 0)
+        self.assertEqual(se.exception.code, 0)
+
+    def test_assertValidPackets_when_OnePacketMatched(self):
+        try:
+            pcap_helper.assertValidPackets("sdas0q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186", 1)
+        except:
+            pytest.fail("no exception expected")
+
+    def test_getClientIpFromFirstPacket_when_IPV4(self):
+        ipv4_packet = scapy_layers.Ether()/scapy_layers.IP(src="127.0.0.2",dst="127.0.0.5")
+        ip_client, ip_server = pcap_helper.getClientServerIpFromFirstPacket(ipv4_packet)
+        self.assertEqual("127.0.0.2", ip_client)
+        self.assertEqual("127.0.0.5", ip_server)
+
+    @pytest.mark.skip(reason="does not run on linux")
+    def test_pcapHandler_typical(self):
+        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        c = [a,b]
+        pcap_helper.pcapHandler(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
+        
+    @pytest.mark.skip(reason="does not run on linux")
+    def test_pcapHandler_when_NoPacketsMatched(self):
+        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        c = [a,b]
+        with self.assertRaises(SystemExit) as se:
+            pcap_helper.pcapHandler(c, "sdas0q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
+        self.assertEqual(se.exception.code, 0)
+        
 if __name__ == "__main__":
     unittest.main()
