6e30c75db123eba7341fd955a96193272c72ee3e
augello <augello@ITMI00DW2267.corp.dom>
2020-06-18 00:04:09 +0200

    get client and server data

--- pcap2sipp/pcap_helper.py
+++ pcap2sipp/pcap_helper.py
@@ -1,6 +1,9 @@
 import scapy.all as scapy
 import scapy.layers.inet as scapy_layers
 import re
+from collections import namedtuple
+
+PeerData = namedtuple("PeerData", "ip port")
 
 def parsePcap(pcap):
     return scapy.rdpcap(pcap)
@@ -14,25 +17,35 @@ def filterPacketsByCallid(packets, callid):
     for packet in packets:
         if isCallIdInPacket(packet, callid):
             filteredPackets.append(packet)
+            scapy.ls(packet)
     return filteredPackets, len(filteredPackets)
-
-#         sport = packet[scapy_layers.UDP].sport
-#         dport = packet[scapy_layers.UDP].dport
-#         if (sport == 5060 or dport == 5060):
-
 
 def getClientServerIpFromFirstPacket(packet):
     return packet[scapy_layers.IP].src, packet[scapy_layers.IP].dst
+
+def getClientServerPortFromFirstPacket(packet):
+    if packet.haslayer(scapy_layers.UDP):
+        return packet[scapy_layers.UDP].sport, packet[scapy_layers.UDP].dport
+    elif packet.haslayer(scapy_layers.TCP):
+        return packet[scapy_layers.TCP].sport, packet[scapy_layers.TCP].dport
+    else:
+        print("PROTOCOL NOT SUPPORTED, ONLY SUPPORTED: UDP/TCP")
+        exit(0)
 
 def assertValidPackets(callid, howManyPackets):
     if howManyPackets == 0:
         print("Call-Id: {} not in pcap".format(callid))
         exit(0)
 
+
+def getClientServerDataFrom(firstPacket):
+    clientIp, serverIp = getClientServerIpFromFirstPacket(firstPacket[0])
+    clientPort, serverPort = getClientServerPortFromFirstPacket(firstPacket[0])
+    client = PeerData(ip=clientIp, port=clientPort)
+    server = PeerData(ip=serverIp, port=serverPort)
+    return client, server
+
 def pcapHandler(packets, callid):
     filteredPackets, howManyPackets = filterPacketsByCallid(packets, callid)
     assertValidPackets(callid, howManyPackets)
-    clientIp, serverIp = getClientServerIpFromFirstPacket(filteredPackets[0])
-    print(len(filteredPackets))
-    for packet in filteredPackets:
-        scapy.ls(packet)
+    client, server = getClientServerDataFrom(filteredPackets[0])
--- tests/pcap_helper_test.py
+++ tests/pcap_helper_test.py
@@ -20,28 +20,28 @@ class Test(unittest.TestCase):
         self.assertTrue(pcap_helper.isCallIdInPacket(a, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186"))
         
     def test_isCallIdInPacket_when_False(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         self.assertFalse(pcap_helper.isCallIdInPacket(a, "sdasdasfassasasd47.186"))
 
     def test_filterPacketsByCallid_when_OnlyOnePacketMatched(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         c = [a,b]
         filtered_packets, num_filtered_packets = pcap_helper.filterPacketsByCallid(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
         self.assertEqual(1, num_filtered_packets)
         self.assertEqual([a], filtered_packets)
         
     def test_filterPacketsByCallid_when_AllPacketsMatched(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"INVITE sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.UDP()/"INVITE sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         c = [a,b]
         filtered_packets, num_filtered_packets = pcap_helper.filterPacketsByCallid(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
         self.assertEqual(2, num_filtered_packets)
         self.assertEqual([a,b], filtered_packets)
         
     def test_filterPacketsByCallid_when_NoPacketsMatched(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: a000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"INVITE sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: b000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: a000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.UDP()/"INVITE sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: b000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         c = [a,b]
         filtered_packets, num_filtered_packets = pcap_helper.filterPacketsByCallid(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
         self.assertEqual(0, num_filtered_packets)
@@ -59,22 +59,48 @@ class Test(unittest.TestCase):
             pytest.fail("no exception expected")
 
     def test_getClientIpFromFirstPacket_when_IPV4(self):
-        ipv4_packet = scapy_layers.Ether()/scapy_layers.IP(src="127.0.0.2",dst="127.0.0.5")
+        ipv4_packet = scapy_layers.IP(src="127.0.0.2",dst="127.0.0.5")
         ip_client, ip_server = pcap_helper.getClientServerIpFromFirstPacket(ipv4_packet)
         self.assertEqual("127.0.0.2", ip_client)
         self.assertEqual("127.0.0.5", ip_server)
+        
+    def test_getClientServerPortFromFirstPacket_when_UDP(self):
+        udp_packet = scapy_layers.UDP(sport=5070,dport=5080)
+        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(udp_packet)
+        self.assertEqual(5070, client_port)
+        self.assertEqual(5080, server_port)
+        
+    def test_getClientServerPortFromFirstPacket_when_TCP(self):
+        tcp_packet = scapy_layers.TCP(sport=5050,dport=5010)
+        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(tcp_packet)
+        self.assertEqual(5050, client_port)
+        self.assertEqual(5010, server_port)
+        
+    def test_getClientServerPortFromFirstPacket_when_ICMP(self):
+        icmp_packet = scapy_layers.ICMP()
+        with self.assertRaises(SystemExit) as se:
+            client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(icmp_packet)
+        self.assertEqual(se.exception.code, 0)
+        
+    def test_getClientServerDataFrom_when_TCP(self):
+        tcp_packet = scapy_layers.IP(src="127.0.0.2",dst="127.0.0.5")/scapy_layers.TCP(sport=5050,dport=5010)
+        client, server = pcap_helper.getClientServerDataFrom(tcp_packet)
+        self.assertEqual(client.ip, "127.0.0.2")
+        self.assertEqual(client.port, 5050)
+        self.assertEqual(server.ip, "127.0.0.5")
+        self.assertEqual(server.port, 5010)
 
     @pytest.mark.skip(reason="does not run on linux")
     def test_pcapHandler_typical(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         c = [a,b]
         pcap_helper.pcapHandler(c, "g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
         
     @pytest.mark.skip(reason="does not run on linux")
     def test_pcapHandler_when_NoPacketsMatched(self):
-        a=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
-        b=scapy_layers.Ether()/scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        a=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
         c = [a,b]
         with self.assertRaises(SystemExit) as se:
             pcap_helper.pcapHandler(c, "sdas0q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186")
