26e4515b75f12973d4e00e8880eaa3d46bc0bfd6
augello <augello@ITMI00DW2267.corp.dom>
2020-06-19 23:43:57 +0200

    Get protocol ip port from first packet

--- pcap2sipp/pcap_helper.py
+++ pcap2sipp/pcap_helper.py
@@ -3,7 +3,8 @@ import scapy.layers.inet as scapy_layers
 import re
 from collections import namedtuple
 
-PeerData = namedtuple("PeerData", "ip port")
+PeerData = namedtuple("PeerData", "ip port protocol")
+PacketInfo = namedtuple("PacketInfo", "packet direction")
 
 def parsePcap(pcap):
     return scapy.rdpcap(pcap)
@@ -23,29 +24,37 @@ def filterPacketsByCallid(packets, callid):
 def getClientServerIpFromFirstPacket(packet):
     return packet[scapy_layers.IP].src, packet[scapy_layers.IP].dst
 
-def getClientServerPortFromFirstPacket(packet):
+def getClientServerPortFromFirstPacket(packet, protocol):
+    return packet[protocol].sport, packet[protocol].dport
+        
+def getClientServerProtocolFromFirstPacket(packet):
     if packet.haslayer(scapy_layers.UDP):
-        return packet[scapy_layers.UDP].sport, packet[scapy_layers.UDP].dport
+        return scapy_layers.UDP
     elif packet.haslayer(scapy_layers.TCP):
-        return packet[scapy_layers.TCP].sport, packet[scapy_layers.TCP].dport
+        return scapy_layers.TCP
     else:
         print("PROTOCOL NOT SUPPORTED, ONLY SUPPORTED: UDP/TCP")
         exit(0)
+
+def getClientServerDataFrom(firstPacket):
+    clientIp, serverIp = getClientServerIpFromFirstPacket(firstPacket[0])
+    protocol = getClientServerProtocolFromFirstPacket(firstPacket[0])
+    clientPort, serverPort = getClientServerPortFromFirstPacket(firstPacket[0], protocol)
+    client = PeerData(ip=clientIp, port=clientPort, protocol=protocol)
+    server = PeerData(ip=serverIp, port=serverPort, protocol=protocol)
+    return client, server
 
 def assertValidPackets(callid, howManyPackets):
     if howManyPackets == 0:
         print("Call-Id: {} not in pcap".format(callid))
         exit(0)
 
-
-def getClientServerDataFrom(firstPacket):
-    clientIp, serverIp = getClientServerIpFromFirstPacket(firstPacket[0])
-    clientPort, serverPort = getClientServerPortFromFirstPacket(firstPacket[0])
-    client = PeerData(ip=clientIp, port=clientPort)
-    server = PeerData(ip=serverIp, port=serverPort)
-    return client, server
+def getSipCallFlowFrom(filteredPackets):
+    for packet in filteredPackets:
+        pass
 
 def pcapHandler(packets, callid):
     filteredPackets, howManyPackets = filterPacketsByCallid(packets, callid)
     assertValidPackets(callid, howManyPackets)
     client, server = getClientServerDataFrom(filteredPackets[0])
+    callFlow = getSipCallFlowFrom(filteredPackets)
--- tests/pcap_helper_test.py
+++ tests/pcap_helper_test.py
@@ -66,20 +66,30 @@ class Test(unittest.TestCase):
         
     def test_getClientServerPortFromFirstPacket_when_UDP(self):
         udp_packet = scapy_layers.UDP(sport=5070,dport=5080)
-        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(udp_packet)
+        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(udp_packet, scapy_layers.UDP)
         self.assertEqual(5070, client_port)
         self.assertEqual(5080, server_port)
         
     def test_getClientServerPortFromFirstPacket_when_TCP(self):
         tcp_packet = scapy_layers.TCP(sport=5050,dport=5010)
-        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(tcp_packet)
+        client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(tcp_packet, scapy_layers.TCP)
         self.assertEqual(5050, client_port)
         self.assertEqual(5010, server_port)
         
-    def test_getClientServerPortFromFirstPacket_when_ICMP(self):
+    def test_getClientServerProtocolFromFirstPacket_when_TCP(self):
+        tcp_packet = scapy_layers.TCP()
+        protocol = pcap_helper.getClientServerProtocolFromFirstPacket(tcp_packet)
+        self.assertEqual(scapy_layers.TCP, protocol)
+        
+    def test_getClientServerProtocolFromFirstPacket_when_UDP(self):
+        udp_packet = scapy_layers.UDP()
+        protocol = pcap_helper.getClientServerProtocolFromFirstPacket(udp_packet)
+        self.assertEqual(scapy_layers.UDP, protocol)
+        
+    def test_getClientServerProtocolFromFirstPacket_when_ICMP(self):
         icmp_packet = scapy_layers.ICMP()
         with self.assertRaises(SystemExit) as se:
-            client_port, server_port = pcap_helper.getClientServerPortFromFirstPacket(icmp_packet)
+            protocol = pcap_helper.getClientServerProtocolFromFirstPacket(icmp_packet)
         self.assertEqual(se.exception.code, 0)
         
     def test_getClientServerDataFrom_when_TCP(self):
@@ -89,6 +99,12 @@ class Test(unittest.TestCase):
         self.assertEqual(client.port, 5050)
         self.assertEqual(server.ip, "127.0.0.5")
         self.assertEqual(server.port, 5010)
+        
+    def test_getSipCallFlowFrom_when_simpleScenario(self):
+        a=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: g000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        b=scapy_layers.IP()/scapy_layers.UDP()/"OPTIONS sip:Fw-NMS-2:5060 SIP/2.0\r\nVia: SIP/2.0/UDP 10.252.47.186:5060;branch=z9hG4bK0g04430050bgj18o80j1\r\nTo: sip:ping@Fw-NMS-2\r\nFrom: <sip:ping@10.252.47.186>;tag=g000000q5m200-jbe0000\r\nCall-ID: h000000q5m2003tedhjqk9l5i1-jbe0000@10.252.47.186\r\nCSeq: 14707 OPTIONS\r\nMax-Forwards: 0\r\nContent-Length: 0\r\n\r\n"
+        packets = [a,b]
+        callFlow = pcap_helper.getSipCallFlowFrom(packets)
 
     @pytest.mark.skip(reason="does not run on linux")
     def test_pcapHandler_typical(self):
