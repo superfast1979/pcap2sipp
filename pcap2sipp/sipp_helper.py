b6d5fea5f7f4331d0581c24d6f997bbeca117cff
augello <augello@ITMI00DW2267.corp.dom>
2020-06-26 23:55:05 +0200

    handling byte strings

--- pcap2sipp/sipp_helper.py
+++ pcap2sipp/sipp_helper.py
@@ -2,14 +2,14 @@ import os
 
 def writeScenarioHeader(path, file):
     with open(os.path.join(path,file), "wb") as scenario:
-        scenario.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
-        scenario.write('<scenario name="{}">\n'.format(file))
+        scenario.write(bytes('<?xml version="1.0" encoding="ISO-8859-1"?>\n'))
+        scenario.write(bytes('<scenario name="{}">\n'.format(file)))
         
 def writeScenarioFooter(path, file):
     with open(os.path.join(path, file), "a+b") as scenario:
-        scenario.write('  <ResponseTimeRepartition value="10, 20, 30, 40, 50, 100, 150, 200"/>\n')
-        scenario.write('  <CallLengthRepartition value="10, 50, 100, 500, 1000, 5000, 10000"/>\n')
-        scenario.write('</scenario>\n')
+        scenario.write(bytes('  <ResponseTimeRepartition value="10, 20, 30, 40, 50, 100, 150, 200"/>\n'))
+        scenario.write(bytes('  <CallLengthRepartition value="10, 50, 100, 500, 1000, 5000, 10000"/>\n'))
+        scenario.write(bytes('</scenario>\n'))
         
 def sippHandler(callFlowFilteredByCallid, path):
     writeScenarioHeader(path, "client_scenario.xml")
