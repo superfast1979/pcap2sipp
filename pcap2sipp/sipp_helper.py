9f8da837ad24abcf17c94ae2975849a8825d7642
augello <augello@ITMI00DW2267.corp.dom>
2020-06-27 00:12:02 +0200

    retry

--- pcap2sipp/sipp_helper.py
+++ pcap2sipp/sipp_helper.py
@@ -2,14 +2,14 @@ import os
 
 def writeScenarioHeader(path, file):
     with open(os.path.join(path,file), "wb") as scenario:
-        scenario.write(bytes('<?xml version="1.0" encoding="ISO-8859-1"?>\n').encode("utf-8"))
-        scenario.write(bytes('<scenario name="{}">\n'.format(file)).encode("utf-8"))
+        scenario.write(bytes(b'<?xml version="1.0" encoding="ISO-8859-1"?>\n'))
+        scenario.write(bytes(b'<scenario name="{}">\n'.format(file)))
         
 def writeScenarioFooter(path, file):
     with open(os.path.join(path, file), "a+b") as scenario:
-        scenario.write(bytes('  <ResponseTimeRepartition value="10, 20, 30, 40, 50, 100, 150, 200"/>\n').encode("utf-8"))
-        scenario.write(bytes('  <CallLengthRepartition value="10, 50, 100, 500, 1000, 5000, 10000"/>\n').encode("utf-8"))
-        scenario.write(bytes('</scenario>\n').encode("utf-8"))
+        scenario.write(bytes(b'  <ResponseTimeRepartition value="10, 20, 30, 40, 50, 100, 150, 200"/>\n'))
+        scenario.write(bytes(b'  <CallLengthRepartition value="10, 50, 100, 500, 1000, 5000, 10000"/>\n'))
+        scenario.write(bytes(b'</scenario>\n'))
         
 def sippHandler(callFlowFilteredByCallid, path):
     writeScenarioHeader(path, "client_scenario.xml")
