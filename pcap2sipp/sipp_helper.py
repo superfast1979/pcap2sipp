b995ee29428b21d25552cf503e33deb72aabf5af
augello <augello@ITMI00DW2267.corp.dom>
2021-11-13 21:28:39 +0100

    first version completed. text based message in pcap.

--- pcap2sipp/sipp_helper.py
+++ pcap2sipp/sipp_helper.py
@@ -1,5 +1,6 @@
 import os
 import settings
+from __builtin__ import True
 
 def writeScenarioHeader(path, file):
     with open(os.path.join(path,file), "wb") as scenario:
@@ -20,18 +21,48 @@ def writeSendMessage(path, file, sipMsg):
         scenario.write(bytes(b'      ]]>\n'))
         scenario.write(bytes(b'  </send>\n\n'))
         
+def writeRecvMessageRequest(path, file, method):
+    with open(os.path.join(path, file), "a+b") as scenario:
+        scenario.write(bytes(b'  <recv request="{}"/>\n\n'.format(method)))
+        
+def writeRecvMessageResponse(path, file, response):
+    with open(os.path.join(path, file), "a+b") as scenario:
+        scenario.write(bytes(b'  <recv response="{}"/>\n\n'.format(response)))
+        
+def isResponse(firstLine):
+    if firstLine.startswith("sip/2.0"):
+        return True
+    return False
+
+REQUEST = 1
+RESPONSE = 2
+
+def parseFirstLineFrom(sipMsg):
+    lines = sipMsg.split("\r\n")
+    if isResponse(lines[0]):
+        return RESPONSE, lines[0].split(" ")[1]
+    else:
+        return REQUEST, lines[0].split(" ")[0]
+    
 def sippHandler(callFlowFilteredByCallid, path):
     writeScenarioHeader(path, "client_scenario.xml")
     writeScenarioHeader(path, "server_scenario.xml")
     for packetInfo in callFlowFilteredByCallid:
         sipMsg = packetInfo.packet.load.lower().decode('utf-8')
+        messageType, method_or_response = parseFirstLineFrom(sipMsg)
         direction = packetInfo.direction
         if direction == settings.CLIENT_TO_SERVER:
             writeSendMessage(path, "client_scenario.xml", sipMsg)
-            #writeRecvMessage(path, "server_scenario.xml", sipMsg)
+            if messageType == REQUEST:
+                writeRecvMessageRequest(path, "server_scenario.xml", method_or_response)
+            else:
+                writeRecvMessageResponse(path, "server_scenario.xml", method_or_response)
         else:
             writeSendMessage(path, "server_scenario.xml", sipMsg)
-            #writeRecvMessage(path, "client_scenario.xml", sipMsg)
+            if messageType == REQUEST:
+                writeRecvMessageRequest(path, "client_scenario.xml", method_or_response)
+            else:
+                writeRecvMessageResponse(path, "client_scenario.xml", method_or_response)
     writeScenarioFooter(path, "client_scenario.xml")
     writeScenarioFooter(path, "server_scenario.xml")
     pass
