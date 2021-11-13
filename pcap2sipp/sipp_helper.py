a38c1179dbd7821ef3c6512c4f3881c9ca8939d0
augello <augello@ITMI00DW2267.corp.dom>
2021-11-13 19:40:54 +0100

    implementing sent messages in sipp

--- pcap2sipp/sipp_helper.py
+++ pcap2sipp/sipp_helper.py
@@ -12,12 +12,26 @@ def writeScenarioFooter(path, file):
         scenario.write(bytes(b'  <CallLengthRepartition value="10, 50, 100, 500, 1000, 5000, 10000"/>\n'))
         scenario.write(bytes(b'</scenario>\n'))
         
+def writeSendMessage(path, file, sipMsg):
+    with open(os.path.join(path, file), "a+b") as scenario:
+        scenario.write(bytes(b'  <send>\n'))
+        scenario.write(bytes(b'      <![CDATA[\n'))
+        scenario.write(bytes(b'{}\n').format(sipMsg))
+        scenario.write(bytes(b'      ]]>\n'))
+        scenario.write(bytes(b'  </send>\n\n'))
+        
 def sippHandler(callFlowFilteredByCallid, path):
     writeScenarioHeader(path, "client_scenario.xml")
-    for packetInfo in callFlowFilteredByCallid:
-        if packetInfo.direction == settings.CLIENT_TO_SERVER:
-            print("CLIENT_TO_SERVER")
-    writeScenarioFooter(path, "client_scenario.xml")
     writeScenarioHeader(path, "server_scenario.xml")
+    for packetInfo in callFlowFilteredByCallid:
+        sipMsg = packetInfo.packet.load.lower().decode('utf-8')
+        direction = packetInfo.direction
+        if direction == settings.CLIENT_TO_SERVER:
+            writeSendMessage(path, "client_scenario.xml", sipMsg)
+            #writeRecvMessage(path, "server_scenario.xml", sipMsg)
+        else:
+            writeSendMessage(path, "server_scenario.xml", sipMsg)
+            #writeRecvMessage(path, "client_scenario.xml", sipMsg)
+    writeScenarioFooter(path, "client_scenario.xml")
     writeScenarioFooter(path, "server_scenario.xml")
     pass
